40af5ac6ac1dc7f754ead41f827346f4c2f46dd1
Cristopher Zaiz <e.zaizortega@gmail.com>
2020-05-28 21:37:54 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    extend: {
+      colors: { 
+        'fondo': '#C3A0FE',
+        'boton': '#E3D2FF',
+        'animation': '#f8f5f5'
+      }
+    },
+    fontFamily: { 
+      'text': ['Quicksand']
+    }
+  },
+  variants: {},
+  plugins: []
+}
